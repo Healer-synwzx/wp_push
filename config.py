3fd46789fda1d1a37420eb345322a66a75046bd1
qi <panjiajia.coder@gmail.com>
2020-05-13 14:40:52 +0800

    修改sqlalchemy连接池

--- config.py
+++ config.py
@@ -22,5 +22,7 @@ MYSQL_DB = os.getenv("mysql_db", "zhihuhaowu")
 SQLALCHEMY_DATABASE_URI = \
     f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4'
 
+SQLALCHEMY_POOL_RECYCLE = 100
+SQLALCHEMY_POOL_SIZE = 10
 
 logger = logging.getLogger('pusher')
